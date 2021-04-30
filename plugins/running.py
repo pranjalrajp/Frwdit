fc3d8771a2ad3c3d1892103fda71b864335ef511
pranjalrajp <76554434+pranjalrajp@users.noreply.github.com>
2021-04-30 17:05:14 +0530

    Update running.py

--- plugins/running.py
+++ plugins/running.py
@@ -41,7 +41,7 @@ async def run(bot, message):
             await bot.copy_message(
                 chat_id=TO,
                 from_chat_id=FROM,       
-                caption=Translation.CAPTION.format(file_name),
+                caption=Translation.CAPTION.format(fitle_name),
                 message_id=message.message_id
             )
             files_count += 1
