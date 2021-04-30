ff188efd07bd61f76365ac7748024c66e9dd3513
pranjalrajp <76554434+pranjalrajp@users.noreply.github.com>
2021-04-30 17:24:36 +0530

    Update running.py

--- plugins/running.py
+++ plugins/running.py
@@ -33,6 +33,7 @@ async def run(bot, message):
             if message.video:
                 file_name = message.video.file_name
 
+                
             else:
                 file_name =
             await bot.copy_message(
