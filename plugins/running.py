03f41bce4cfae4480a46a377dbc4512766f15048
pranjalrajp <76554434+pranjalrajp@users.noreply.github.com>
2021-05-01 13:13:36 +0530

    Update running.py

--- plugins/running.py
+++ plugins/running.py
@@ -33,7 +33,7 @@ async def run(bot, message):
             if message.video:
                 file_name = message.video.file_name
             elif message.document:
-                file_name = message.document.size
+                file_name = message.document._
             elif message.audio:
                 file_name = message.audio.file_name
             else:
